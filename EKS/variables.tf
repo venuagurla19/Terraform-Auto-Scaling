bcb322cb23c8e28c86a2cd3161e0611299c9adbb
venu <agurlavenu1@gmail.com>
2025-04-04 13:54:02 +0530

    veblihk

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -52,13 +52,13 @@ variable "node_group" {
         })
     }))
     default = {
-    general = {
-        instance_types = ["t3.medium"]
-        capacity_type  = "ON_DEMAND"
-        scaling_config = {
-        desired_size = 2
-        max_size     = 4
-        min_size     = 1
+        general = {
+            instance_types = ["t3.medium"]
+            capacity_types  = "ON_DEMAND"
+            scaling_config = {
+                desired_size = 2
+                max_size     = 4
+                min_size     = 1
         }
     }
     }
