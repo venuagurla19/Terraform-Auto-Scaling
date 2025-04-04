9f359a553b91833384abf5f879a051743044e996
venu <agurlavenu1@gmail.com>
2025-04-04 13:06:23 +0530

    vebkk

--- EKS/main.tf
+++ EKS/main.tf
@@ -23,17 +23,17 @@ provider "aws" {
 module "vpc" {
             source = "./modules/vpc"
 
-            vpc_cidr             = var.vpc_cidr
-            availability_zones   = var.availability_zone
-            private_subnet_cidrs = var.private_subnet_cidrs
-            public_subnet_cidrs  = var.public_subnet_cidrs
-            cluster_name         = var.cluster_name
-        }
+        vpc_cidr             = var.vpc_cidr
+        availability_zone   = var.availability_zone
+        private_subnet_cidrs = var.private_subnet_cidrs
+        public_subnet_cidrs  = var.public_subnet_cidrs
+        cluster_name         = var.cluster_name
+    }
 
 module "eks" {
     source         = "./modules/eks"
     cluster_name   = var.cluster_name
     cluster_version = var.cluster_version
     subnet_ids     = module.vpc.private_subnet_ids
-    node_group     = var.node_group  # Correct name
+    node_group     = var.node_group # Correct name
     }
